a04b978789d164c5190a5fa91670cf1f7ca42692
Blowing_up_time <Zac@pcncloud.onmicrosoft.com>
2021-09-06 15:55:09 +1000

    Added Pullwork
    shutup

---
+++ PullWork/pullWorkImage.py
@@ -0,0 +1,20 @@
+import argparse
+import os
+
+#setup arguments
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file")
+
+args = parser.parse_args()
+
+# Filepath
+filename = args.file
+filepath = ""
+if filename.endswith(".jpg") | filename.endswith(".png"):
+	filepath = "/sdcard/DCIM/Camera/" + filename
+else:
+	filepath = "/sdcard/DCIM/Camera/" + filename + ".jpg"
+
+# RUn command
+os.system("adb pull " + filepath + " .")
---
+++ PullWork/pullimg.bat
@@ -0,0 +1,3 @@
+@echo off
+
+py -3.6 pullWorkImage.py %1
